edf66670880f8fd9b89cd236649b6e7ef8613e9a
Marco Rossini <marco.rossini@protonmail.ch>
2022-03-10 06:44:02 +0100

    Removed custom functions and replaced with digit-char-p.

--- c-string-literals.lisp
+++ c-string-literals.lisp
@@ -6,63 +6,11 @@
        (cond
          ,@(loop for c in cases collect `((char-equal ,(first c) ,ch) ,@(rest c)))))))
 
-(defun oct-p (c)
-  (or (char= c #\0)
-      (char= c #\1)
-      (char= c #\2)
-      (char= c #\3)
-      (char= c #\4)
-      (char= c #\5)
-      (char= c #\6)
-      (char= c #\7)))
-
-(defun hex-p (c)
-  (or (oct-p c)
-      (char= c #\8)
-      (char= c #\9)
-      (char-equal c #\a)
-      (char-equal c #\b)
-      (char-equal c #\c)
-      (char-equal c #\d)
-      (char-equal c #\e)
-      (char-equal c #\f)))
-
-(defun oct-digit (c)
-  (char-case c
-    (#\0 0)
-    (#\1 1)
-    (#\2 2)
-    (#\3 3)
-    (#\4 4)
-    (#\5 5)
-    (#\6 6)
-    (#\7 7)
-    (#\8 8)))
-
-(defun hex-digit (c)
-  (char-case c
-    (#\0 0)
-    (#\1 1)
-    (#\2 2)
-    (#\3 3)
-    (#\4 4)
-    (#\5 5)
-    (#\6 6)
-    (#\7 7)
-    (#\8 8)
-    (#\9 9)
-    (#\a 10)
-    (#\b 11)
-    (#\c 12)
-    (#\d 13)
-    (#\e 14)
-    (#\f 15)))
-
 (defun oct-to-int (c1 c2 c3)
-  (+ (ash (oct-digit c1) 6) (ash (oct-digit c2) 3) (oct-digit c3)))
+  (+ (ash (digit-char-p c1 8) 6) (ash (digit-char-p c2 8) 3) (digit-char-p c3)))
 
 (defun hex-to-int (c1 c2)
-  (+ (ash (hex-digit c1) 4) (hex-digit c2)))
+  (+ (ash (digit-char-p c1 16) 4) (digit-char-p c2 16)))
 
 (defun read-string (stream delim)
   (let (backslash lst oct hex)
@@ -72,15 +20,15 @@
                ;; Octal code
                (oct
                 (cond
-                  ((and (= (length oct) 2) (oct-p c)) (push (code-char (oct-to-int (cadr oct) (car oct) c)) lst) (setf oct nil))
-                  ((and (< (length oct) 2) (oct-p c)) (push c oct))
+                  ((and (= (length oct) 2) (digit-char-p c 8)) (push (code-char (oct-to-int (cadr oct) (car oct) c)) lst) (setf oct nil))
+                  ((and (< (length oct) 2) (digit-char-p c 8)) (push c oct))
                   ((and (= (length oct) 1) (char= (car oct) #\0)) (push (code-char 0) lst) (unread-char c stream) (setf oct nil))
                   (t (error "Illegal octal code in c-style string escape sequence!"))))
                ;; Hex code
                (hex
                 (cond
-                  ((and (= (length hex) 2) (hex-p c)) (push (code-char (hex-to-int (car hex) c)) lst) (setf hex nil))
-                  ((and (< (length hex) 2) (hex-p c)) (push c hex))
+                  ((and (= (length hex) 2) (digit-char-p c 16)) (push (code-char (hex-to-int (car hex) c)) lst) (setf hex nil))
+                  ((and (< (length hex) 2) (digit-char-p c 16)) (push c hex))
                   (t (error "Illegal hex code in c-style string escape sequence!"))))
                ;; Escaped characters
                (backslash
@@ -95,7 +43,7 @@
                   ((char= c #\r) (push #\Return lst))
                   ((char= c #\t) (push #\Tab lst))
                   ((char= c #\v) (push (code-char 11) lst))
-                  ((oct-p c) (when (> (oct-digit c) 3) (error "Illegal octal code in c-style string escape sequence!")) (push c oct))
+                  ((digit-char-p c 8) (when (> (digit-char-p c 8) 3) (error "Illegal octal code in c-style string escape sequence!")) (push c oct))
                   ((char= c #\x) (push c hex))
                   (t (push c lst))))
                ;; Non-escaped characters
